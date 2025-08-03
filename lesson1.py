cfc33e2935dfaa1777805f88b863f0de137865fa
Volverstone <tilek37@mail.ru>
2025-08-03 16:41:16 +0600

    bebebe

--- lesson1.py
+++ lesson1.py
@@ -1,36 +1,14 @@
-vowels = "AEIOUАЕЁИОУЫЭЮЯ"
-consonants = "BCDFGHJKLMNPQRSTVWXYZБВГДЖЗЙКЛМНПРСТФХЦЧШЩ"
+user = int(input('Введите число'))+1
+type_of_num = int(input('2 четное 1 нечетное'))
 
-while True:
-    word = input("Слово (или 'exit' для выхода): ").upper()
+if type_of_num == 2:
+    for i in range(1, user):
+        if i % 2 == 0:
+            print(i, end=' ')
 
-    if word == "EXIT":
-        print("Программа завершена")
-        break
 
-    count_letters = 0
-    vowel_count = 0
-    consonant_count = 0
+if type_of_num == 1:
+    for i in range(1, user):
+        if i % 2 != 0:
+            print(i, end=' ')
 
-    for letter in word:
-        if letter.isalpha():
-            count_letters += 1
-
-            if letter in vowels:
-                vowel_count += 1
-            elif letter in consonants:
-                consonant_count += 1
-
-    print(f"\nСлово: {word}")
-    print(f"Всего букв: {count_letters}")
-    print(f"Гласных: {vowel_count}")
-    print(f"Согласных: {consonant_count}")
-
-    if count_letters > 0:
-        vowels_percent = (vowel_count / count_letters) * 100
-        consonants_percent = (consonant_count / count_letters) * 100
-
-        print(f"Гласные: {vowels_percent:.2f}%")
-        print(f"Согласные: {consonants_percent:.2f}%\n")
-    else:
-        print("В слове нет букв!\n")
